79c593582cacd853b4ffca3488e2a8e0614e8500
johnnyBuildsIt <bluegrassandclimbing@hotmail.com>
2019-07-07 21:13:33 -0400

    player displaying

--- js/app.js
+++ js/app.js
@@ -24,8 +24,10 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-const Player = function() {
-
+const Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
 }
 
 Player.prototype.update = function(){
@@ -33,7 +35,7 @@ Player.prototype.update = function(){
 }
 
 Player.prototype.render = function(){
-
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 Player.prototype.handleInput = function(){
@@ -44,8 +46,8 @@ Player.prototype.handleInput = function(){
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-let allEnemies = []
-let player = new Player();
+let allEnemies = [new Enemy];
+let player = new Player(200, 375);
 
 
 // This listens for key presses and sends the keys to your
